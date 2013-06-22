f3141627501eb61aa116f4de5bb4a193c9d64b2c
achhhapar <ahunawar@hotmail.com>
2013-06-21 17:34:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroTwitter::Application.config.secret_token = '57d53f781e33e925572d57d45fd8a8c26b6b565a34b619b37267b04b2d33ad439382638b45ed99949c451ce80153f19b769762c1b91a2c9f1bc08ab95d490c51'
